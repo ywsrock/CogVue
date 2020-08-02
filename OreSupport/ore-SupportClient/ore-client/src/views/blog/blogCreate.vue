02841e7e99aa331e5aa6c0b1ed927c9842a53a8a
Ryusei <a1908rk@aiit.ac.jp>
2020-08-02 20:20:23 +0900

    blog

---
+++ OreSupport/ore-SupportClient/ore-client/src/views/blog/blogCreate.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="ユーザINFO">
+    <form @submit.prevent="createBlog" style="border:1px solid #ccc">
+      <div class="container">
+        <h1>ブログ登録</h1>
+        <p>ブログ新規作成</p>
+        <hr />
+        <label for="title"><b>タイトル</b></label>
+        <input
+          type="text"
+          ref="title"
+          placeholder="タイトル"
+          v-model="registForm.title"
+          name="title"
+          required
+        />
+        <label for="content"><b>内容</b></label>
+        <input
+          type="text"
+          ref="content"
+          placeholder="内容"
+          name="content"
+          v-model="registForm.content"
+          
+          required
+        />
+        <!-- <label for="repeatpassword"><b>その他</b></label>
+        <input
+          type="text"
+          ref="repeatpassword"
+          placeholder="その他"
+          name="repeatpassword"
+          v-model="registForm.repeatpassword"
+          
+          required
+        /> -->
+        <!-- <p style="color:red;font-size:12px" ref="e-password2"></p>
+
+        <label>
+          <input
+            type="checkbox"
+            checked="checked"
+            name="remember"
+            style="margin-bottom:15px"
+          />
+          
+        </label> -->
+
+        <div class="clearfix">
+          <button type="button" class="cancelbtn">キャンセル</button>
+          <button type="submit" class="signupbtn">新規作成</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Message } from "element-ui";
+
+/* eslint-disable */
+
+export default {
+  data() {
+    return {
+      registForm: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    // blog登録処理ハンドラー
+    createBlog: function(e) {
+      // 二重コミット防止のため、ボタンを非活性
+      e.target.disabled=true;
+      // // 全項目チェック、問題なければ、登録処理を行う
+      // const isValid = this.validate(this.registForm, this.rules);
+      // if (!isValid) {
+        // ユーザ登録処理
+         this.$store
+          .dispatch("blog/createBlog", this.registForm)
+          .then((res) => {
+            Message({
+              message: "作成OK",
+              type: "success",
+              duration: 5 * 1000,
+            }),
+              // ログイン画面に遷移
+              this.$router.push({ path: "/login" }).catch(err => {});
+          })
+          .catch((error) => {
+             e.target.disabled=false;
+            console.log(error.data);
+            console.log("作成失敗");
+          });
+      }
+    },
+};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+/* Full-width input fields */
+input[type="text"],
+input[type="password"] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type="text"]:focus,
+input[type="password"]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4caf50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity: 1;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  padding: 14px 20px;
+  background-color: #f44336;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.cancelbtn,
+.signupbtn {
+  float: left;
+  width: 50%;
+}
+
+/* Add padding to container elements */
+.container {
+  padding: 16px;
+}
+
+/* Clear floats */
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+  .cancelbtn,
+  .signupbtn {
+    width: 100%;
+  }
+}
+</style>
---
+++ OreSupport/ore-SupportClient/ore-client/src/views/blog/blog_edit.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <pre>
+    <h1>ブログ編集</h1>
+  </pre>title :
+    <p>{{blogDetail.title}}</p>ブログ詳細
+
+<el-input
+  type="text"
+  placeholder="Please input"
+  v-model="blogDetail.title"
+  maxlength="30"
+  show-word-limit
+>
+</el-input>
+<div style="margin: 20px 0;"></div>
+<el-input
+  type="textarea"
+  placeholder="Please input"
+  v-model="blogDetail.content"
+  maxlength="2000"
+  show-word-limit
+>
+</el-input>
+<el-button  size="mini" @click="blogUpdate">Update</el-button>
+  
+  </div>
+</template>
+<script>
+import { Message } from "element-ui";
+export default {
+  data() {
+    return {
+      blogDetail: {
+        id:"",
+        title: "",
+        content: ""
+      }
+    };
+  },
+  mounted() {
+    var that = this;
+    // console.log(this.$router.query); 次回の閻餐会
+    this.$nextTick().then(function() {
+      const content = that.$store.getters.get_content;
+      const title = that.$store.getters.get_title;
+      const id = that.$store.getters.get_id;
+      that.blogDetail.content = content;
+      that.blogDetail.title = title;
+      that.blogDetail.id = id;
+    });
+  },
+
+  methods:{
+
+
+    blogUpdate: function(e) {
+      // 二重コミット防止のため、ボタンを非活性
+      e.target.disabled=true;
+      // // 全項目チェック、問題なければ、登録処理を行う
+      // const isValid = this.validate(this.registForm, this.rules);
+      // if (!isValid) {
+        // ユーザ登録処理
+        console.log(`val = ${JSON.stringify(this.$route.query.id)}`)
+        this.$store
+          .dispatch("blog/blogUpdate", this.blogDetail)
+          .then((res) => {
+            Message({
+              message: "作成OK",
+              type: "success",
+              duration: 5 * 1000,
+            }),
+              // ブログリスト画面に遷移
+              this.$router.push("/blogList");
+          })
+          .catch((error) => {
+            e.target.disabled=false;
+            console.log(error.data);
+            console.log("作成失敗");
+          });
+      },
+
+  }
+
+
+};
+</script>
