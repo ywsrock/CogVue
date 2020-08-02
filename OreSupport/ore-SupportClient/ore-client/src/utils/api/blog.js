4c44fa04ea50c2423f29e60aef7aa73a9cb63692
Ryusei <a1908rk@aiit.ac.jp>
2020-08-02 22:14:02 +0900

    blog

---
+++ OreSupport/ore-SupportClient/ore-client/src/utils/api/blog.js
@@ -0,0 +1,62 @@
+import request from '@/utils/request'
+
+
+
+
+/**
+ * ユーザ登録
+ * @param data Object
+ * @returns String
+ */
+export function createBlog(data) {
+    return request({
+        url: '/blog/create',
+        method: 'post',
+        data
+    })
+}
+
+/**
+ * ブログ取得
+ * @returns String
+ */
+export function getBlogList() {
+    return request({
+        url: '/blog/bloglist',
+        method: 'get',
+    })
+}
+
+
+export function getBlogDetail(blogID) {
+    return request({
+        url: '/blog/blogdetail',
+        method: 'get',
+        params: { blogID }
+    })
+}
+
+export function blogDelete(blogID) {
+    return request({
+        url: '/blog/blogDelete',
+        method: 'get',
+        params: { blogID }
+    })
+}
+
+export function viewBlog(token) {
+    return request({
+        url: '/blog/view',
+        method: 'get',
+        params: { token }
+        
+    })
+}
+
+export function blogUpdate(data) {
+    return request({
+        url: '/blog/blogUpdate',
+        method: 'post',
+        data
+    })
+}
