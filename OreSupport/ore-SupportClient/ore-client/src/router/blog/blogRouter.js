5056e86fca3e2c642db4cfb8d849e61aa71bdeb1
Ryusei <a1908rk@aiit.ac.jp>
2020-08-02 20:29:07 +0900

    blog router

---
+++ OreSupport/ore-SupportClient/ore-client/src/router/blog/blogRouter.js
@@ -0,0 +1,20 @@
+/* 
+ *  ユーザプロフィール編集用ルータ定義 
+ */
+
+
+import createBlog from "@/views/blog/blogCreate.vue"
+//  import blogList from "@/views/blog/blog_list.vue"
+//  import blogDetail from "@/views/blog/blog_detail.vue"
+
+const blogRouter = {
+    
+    path:"/createBlog",
+    name:"createBlog",
+    components:{
+        default:createBlog
+        },
+    };
+
+
+export default blogRouter
