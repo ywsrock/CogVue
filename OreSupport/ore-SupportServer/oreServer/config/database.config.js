434a3349508b717b5a56617703d43cdee445d925
“junya-ori” <junya.habu@orithankyou.co.jp>
2020-10-30 03:52:45 +0900

    modelのテーブル名の指定を修正

--- OreSupport/ore-SupportServer/oreServer/config/database.config.js
+++ OreSupport/ore-SupportServer/oreServer/config/database.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    connectionLimit: 20,    //Poolの数設定
-    host: 'localhost',      //DBサーバのアドレス
-    database: 'PBL',        //DB名
-    user: 'root',           // ユーザ名
-    password: 'passw0rd',   // パスワー
-    dialect: 'mysql',       // データエースタイプ
-    pool_max: 10,            // プールの最大数
-    pool_min: 5,             // プールの最大数 
-    idle: 20000,             // リソース開放タイムアウト
-    freezeTableName: false  // テーブル名自動複数しない
-}
+  connectionLimit: 20, //Poolの数設定
+  host: "localhost", //DBサーバのアドレス
+  database: "pbl", //DB名
+  user: "root", // ユーザ名
+  password: "passw0rd", // パスワー
+  dialect: "mysql", // データエースタイプ
+  pool_max: 10, // プールの最大数
+  pool_min: 5, // プールの最大数
+  idle: 20000, // リソース開放タイムアウト
+  freezeTableName: false, // テーブル名自動複数しない
+};
