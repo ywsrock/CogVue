52aeccea5f493bd842368511c105152fb2d49585
“junya-ori” <junya.habu@orithankyou.co.jp>
2020-11-08 15:44:33 +0900

    検索パラメーターの取得

--- OreSupport/ore-SupportClient/ore-client/src/views/blog/search_form_select_datebox.vue
+++ OreSupport/ore-SupportClient/ore-client/src/views/blog/search_form_select_datebox.vue
@@ -2,9 +2,9 @@
   <div class="col-sm-12 col-md-6">
     <div class="form-group" style="display: flex;">
       <p class="search-form-title"><span style="margin: 0 auto;">{{ title }}</span></p>
-      <input type="date" id="date" class="search-form-input">
+      <input type="date" id="date" class="search-form-input" ref="from" @change="onChange">
       <span style="padding: 16px 15px; color: black;"> 〜 </span> 
-      <input type="date" id="date" class="search-form-input">
+      <input type="date" id="date" class="search-form-input" ref="to" @change="onChange">
     </div>
   </div>
 </template>
@@ -17,6 +17,12 @@ export default {
       type: String,
       required: true      
     },
+  },
+  methods: {
+    onChange: function(){
+      const params = { from: this.$refs.from.value, to: this.$refs.to.value }
+      this.$emit("emitSelectDate", params);
+    }
   }
 };
 </script>
--- OreSupport/ore-SupportClient/ore-client/src/views/blog/search_form_selectbox.vue
+++ OreSupport/ore-SupportClient/ore-client/src/views/blog/search_form_selectbox.vue
@@ -2,7 +2,7 @@
   <div class="col-sm-12 col-md-6">
     <div class="form-group" style="display: flex;">
       <p class="search-form-title"><span style="margin: 0 auto;">{{ title }}</span></p>
-      <select class="form-control search-form-select" :title="title">
+      <select class="form-control search-form-select" :title="title" ref="option" @change="onChange">
         <template v-for="option in options">
           <option :key="option">{{ option }}</option>
         </template>
@@ -23,6 +23,15 @@ export default {
       required: true
     },
   },
-
+  methods: {
+    onChange: function(){
+      if (this.title === '住所') {
+        this.$emit("emitSelectPref", this.$refs.option.value);
+      }
+      if (this.title === '年代') {
+        this.$emit("emitSelectAge", this.$refs.option.value);
+      }
+    }
+  }
 };
 </script>
